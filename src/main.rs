326793820be9293ceeca8849deb881a49a6fb7d1
Mathias Schuh <mathias.schuh@prosenio.com>
2023-05-11 11:40:42 +0200

    move from easy curl to reqwest for post+json

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,62 @@
-use curl::easy::Easy;
 use serde::{Deserialize, Serialize};
 use serde_yaml::{self};
 use std::fs;
+use std::collections::HashMap;
+use std::str;
+
 #[derive(Debug, Serialize, Deserialize)]
 struct Config {
     path: String,
     file_limit: u32,
     get_url: String,
+    payload_title: String
 }
 
 fn main() {
     let f = std::fs::File::open("config.yml").expect("Could not open file: config.yml");
-    let scrape_config: Config = serde_yaml::from_reader(f).expect("Could not read values.");
-    //println!("{:?}", scrape_config);
+    let config: Config = serde_yaml::from_reader(f).expect("Could not read values.");
+    //println!("{:?}", config);
 
-    let paths = fs::read_dir(scrape_config.path).unwrap();
+    let paths = fs::read_dir(config.path.clone()).unwrap();
     let file_count = paths.count();
     //println!("Found Files: {}", file_count);
 
-    if file_count <= scrape_config.file_limit as usize {
+    if file_count <= config.file_limit as usize {
         println!("File count is less or equal than limit. Exiting..");
         return;
     }
 
-    let mut url = scrape_config.get_url.clone();
-    url.push_str("&file_count=");
-    url.push_str(&file_count.to_string());
+    
+    let mut body = String::from("");
+    body.push_str("Es wurden ");
+    body.push_str(&(file_count.to_string()));
+    body.push_str(" Dateien im Ordner ");
+    body.push_str(&config.path);
+    body.push_str(" gefunden. Das Limit ist: ");
+    body.push_str(&(config.file_limit.to_string()));
 
-    let mut easy = Easy::new();
-    easy.get(true).unwrap();
-    easy.url(&url).unwrap();
-    easy.perform().unwrap();
-    println!("File count is greater than limit. Requested..");
+    let body_str: &str = &body;
+    let title: &str = &config.payload_title.as_str();
+
+    let mut map = HashMap::new();
+    map.insert("@type", "MessageCard");
+    map.insert("@context", "<http://schema.org/extensions>");
+    map.insert("summary", "Meine erste Alert-Summary!");
+    map.insert("themeColor", "D70000");
+    map.insert("title", title);
+    map.insert("text", body_str);
+
+    let _res = rest(config.get_url, map);
+
+    return;
+}
+
+#[tokio::main]
+async fn rest(url: String, map: HashMap<&str, &str>) -> Result<(), Box<dyn std::error::Error>> {
+    let client = reqwest::Client::new();
+    let _res = client.post(url)
+        .json(&map)
+        .send()
+        .await?;
+        Ok(())
 }
